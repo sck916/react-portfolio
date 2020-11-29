a55338758242bb373d074c7affef7c8e1e06a568
Steve Kavuu <mrkavuu@gmail.com>
2020-11-29 17:09:10 -0600

    mo

--- portofilo/src/HomePage.js
+++ portofilo/src/HomePage.js
@@ -30,25 +30,25 @@ function Homepage() {
   </div>
 
   <div className="card" style={{width: '400px', marginTop:'70px', margin:'auto'}}>
-    <h1 className="card-title">About Me</h1>
-    <img className="card-img-top" src="../public/images/1.jpeg" alt="Card image cap" id="img1" />
+    <h1 className="card-title">Steve Kavuu</h1>
+    {/* <img className="card-img-top" src="./public/images/1.jpeg" alt="Card image cap" id="img1" /> */}
     <div className="card-body">
       <p className="card-text">
         Hello, my name is Steve. I am currently a student at Northwestern University's coding program. I am also a
         Husband, Father and Navy veteran. Below is some of my work.
 
       <div>
-          <a href="https://github.com/sck916/">GitHub profile</a>
+          <a href="https://github.com/sck916/" target="_blank">GitHub</a>
           <br />
-          <a href="https://www.linkedin.com/in/steve-kavuu-8a96611b7/">LinkedIn profile</a>
+          <a href="https://www.linkedin.com/in/steve-kavuu-8a96611b7/" target="_blank">LinkedIn</a>
           <br />
-          <a href="https://sck916.github.io/code-refhomework/" target="_blank">Refactor</a>
+          <a href="https://budget21.herokuapp.com/" target="_blank">Budget</a>
           <br />
-          <a href="https://sck916.github.io/responsivehw/" target="_blank">responsive</a>
+          <a href="https://fitnesstracker13.herokuapp.com/" target="_blank">Fitness Tracker</a>
           <br />
           <a href="https://sck916.github.io/Password-Generator/" target="_blank">Password Generator</a>
           <br />
-          <a href=" https://sck916.github.io/code-quiz/" target="_blank">Code Quiz</a>
+          <a href="https://young-basin-59453.herokuapp.com/" target="_blank">Employee Directory</a>
       </div>
 
 
